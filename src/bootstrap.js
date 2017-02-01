52f30ad47aea6050f160b70d92fda1991f194395
Taylor Birkeland <tabirkeland@gmail.com>
2017-01-31 20:41:52 -0500

    Initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,11 @@
+// Lodash
+window._ = require('lodash');
+
+// jQuery
+window.$ = window.jQuery = require('jquery');
+
+// Materialize
+require('materialize-css');
+
+// Type.js
+require('typed.js');
---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+let mix = require('laravel-mix');
+
+mix
+	// .setPublicPath('dist/')
+	.js('src/app.js', 'dist/')
+	.copy('src/img', 'dist/img')
+   	.sass('src/app.scss', 'dist/');
+
+// Full API
+// mix.js(src, output);
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.less(src, output);
+// mix.combine(files, destination);
+// mix.copy(from, to);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
